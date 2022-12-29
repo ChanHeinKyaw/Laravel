f8ddbfefbb2e152c44e251879c51d1a78a430f1e
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2022-12-29 16:36:04 +0630

    drawback-of-static-blog

---
+++ Chapter-1 Solid Basic Concepts/resources/views/blog.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/blog">First blog</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora beatae earum, sint, aut numquam itaque
+            repellat animi inventore magnam optio saepe. Voluptatem incidunt sed earum iste labore accusantium sequi
+            itaque commodi, suscipit tempore et veniam ipsam eius sit doloremque a veritatis deleniti, mollitia neque
+            modi aperiam eum amet. Nisi commodi ex culpa nam! Voluptas eligendi laborum nobis temporibus delectus esse
+            tempore quidem est iure, adipisci quod, eum enim ea culpa odio consequuntur error dicta ab eveniet fugiat
+            voluptatum? Culpa nam cumque quidem in, aut vero, explicabo cupiditate quae, itaque asperiores consequatur?
+            Eveniet, quam consectetur. Ullam dolorum minima nemo cumque quam?
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+    <script src="/js/app.js"></script>
+</body>
+
+</html>
