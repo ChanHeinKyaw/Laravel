b82d465d1fa68de9de5fb794a80780be37796569
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2023-04-05 16:22:36 +0630

    fetch all blogs use file facades

--- Chapter-1 Solid Basic Concepts/resources/views/blogs.blade.php
+++ Chapter-1 Solid Basic Concepts/resources/views/blogs.blade.php
@@ -10,44 +10,9 @@
 </head>
 
 <body>
-    <article>
-        <h1><a href="/blogs/first-blog">First blog</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora beatae earum, sint, aut numquam itaque
-            repellat animi inventore magnam optio saepe. Voluptatem incidunt sed earum iste labore accusantium sequi
-            itaque commodi, suscipit tempore et veniam ipsam eius sit doloremque a veritatis deleniti, mollitia neque
-            modi aperiam eum amet. Nisi commodi ex culpa nam! Voluptas eligendi laborum nobis temporibus delectus esse
-            tempore quidem est iure, adipisci quod, eum enim ea culpa odio consequuntur error dicta ab eveniet fugiat
-            voluptatum? Culpa nam cumque quidem in, aut vero, explicabo cupiditate quae, itaque asperiores consequatur?
-            Eveniet, quam consectetur. Ullam dolorum minima nemo cumque quam?
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/blogs/second-blog">Second blog</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora beatae earum, sint, aut numquam itaque
-            repellat animi inventore magnam optio saepe. Voluptatem incidunt sed earum iste labore accusantium sequi
-            itaque commodi, suscipit tempore et veniam ipsam eius sit doloremque a veritatis deleniti, mollitia neque
-            modi aperiam eum amet. Nisi commodi ex culpa nam! Voluptas eligendi laborum nobis temporibus delectus esse
-            tempore quidem est iure, adipisci quod, eum enim ea culpa odio consequuntur error dicta ab eveniet fugiat
-            voluptatum? Culpa nam cumque quidem in, aut vero, explicabo cupiditate quae, itaque asperiores consequatur?
-            Eveniet, quam consectetur. Ullam dolorum minima nemo cumque quam?
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/blogs/third-blog">Third blog</a></h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora beatae earum, sint, aut numquam itaque
-            repellat animi inventore magnam optio saepe. Voluptatem incidunt sed earum iste labore accusantium sequi
-            itaque commodi, suscipit tempore et veniam ipsam eius sit doloremque a veritatis deleniti, mollitia neque
-            modi aperiam eum amet. Nisi commodi ex culpa nam! Voluptas eligendi laborum nobis temporibus delectus esse
-            tempore quidem est iure, adipisci quod, eum enim ea culpa odio consequuntur error dicta ab eveniet fugiat
-            voluptatum? Culpa nam cumque quidem in, aut vero, explicabo cupiditate quae, itaque asperiores consequatur?
-            Eveniet, quam consectetur. Ullam dolorum minima nemo cumque quam?
-        </p>
-    </article>
+    <?php foreach($blogs as $blog): ?>
+        <?= $blog ?>
+    <?php endforeach; ?>
     <script src="/js/app.js"></script>
 </body>
 
