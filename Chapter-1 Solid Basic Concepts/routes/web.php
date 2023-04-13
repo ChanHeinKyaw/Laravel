9edc525b5a6711b52c96ec82d7404a3750444242
chanheinkyaw <chanheinkyaw.mm@gmail.com>
2023-04-13 17:36:00 +0630

    find a blog by slug

--- Chapter-1 Solid Basic Concepts/routes/web.php
+++ Chapter-1 Solid Basic Concepts/routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
     return view('blogs', ['blogs' => Blog::all()]);
 });
 
-Route::get('/blogs/{blog}', function (Blog $blog) {
+Route::get('/blogs/{blog:slug}', function (Blog $blog) {
     return view('blog', ['blog' => $blog]);
 })->where('blog', '[A-z\d\-_]+');
