e382d9d51c107325078841450967193b474ce5f1
chanheinkyaw <chanheinkyaw.mm@gmail.com>
2023-04-15 00:51:36 +0630

    learn more about eager loading or lazy loading

--- Chapter-1 Solid Basic Concepts/routes/web.php
+++ Chapter-1 Solid Basic Concepts/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     // DB::listen(function($query){
     //     Log::info($query->sql);
     // });
-    return view('blogs', ['blogs' => Blog::with('category','user')->get()]);
+    return view('blogs', ['blogs' => Blog::all()]);
 });
 
 Route::get('/blogs/{blog:slug}', function (Blog $blog) {
@@ -30,10 +30,12 @@ Route::get('/blogs/{blog:slug}', function (Blog $blog) {
 })->where('blog', '[A-z\d\-_]+');
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    // return view('blogs', ['blogs' => $category->blogs->load('category','author')]);
     return view('blogs', ['blogs' => $category->blogs]);
 });
 
 Route::get('/users/{user}', function (User $user) {
+    // return view('blogs', ['blogs' => $user->blogs->load('category','author')]);
     return view('blogs', ['blogs' => $user->blogs]);
 });
 
