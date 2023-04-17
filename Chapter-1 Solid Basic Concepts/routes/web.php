d6706f5b15568be09d3f0672767f4adf1e63c771
chanheinkyaw <chanheinkyaw.mm@gmail.com>
2023-04-17 15:03:52 +0630

    make category dropdown dynamic

--- Chapter-1 Solid Basic Concepts/routes/web.php
+++ Chapter-1 Solid Basic Concepts/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     // DB::listen(function($query){
     //     Log::info($query->sql);
     // });
-    return view('blogs', ['blogs' => Blog::latest()->get()]);
+    return view('blogs', ['blogs' => Blog::latest()->get(), 'categories' => Category::all()]);
 });
 
 Route::get('/blogs/{blog:slug}', function (Blog $blog) {
@@ -31,11 +31,11 @@ Route::get('/blogs/{blog:slug}', function (Blog $blog) {
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     // return view('blogs', ['blogs' => $category->blogs->load('category','author')]);
-    return view('blogs', ['blogs' => $category->blogs]);
+    return view('blogs', ['blogs' => $category->blogs,'categories' => Category::all(), 'currentCategory' => $category]);
 });
 
 Route::get('/users/{user:username}', function (User $user) {
     // return view('blogs', ['blogs' => $user->blogs->load('category','author')]);
-    return view('blogs', ['blogs' => $user->blogs]);
+    return view('blogs', ['blogs' => $user->blogs,'categories' => Category::all()]);
 });
 
