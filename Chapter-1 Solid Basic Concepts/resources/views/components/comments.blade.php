fbd5c95aadba43ca5e2f1fe732913ef398e00398
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2023-07-14 23:54:58 +0630

    how to work with component attributes

--- Chapter-1 Solid Basic Concepts/resources/views/components/comments.blade.php
+++ Chapter-1 Solid Basic Concepts/resources/views/components/comments.blade.php
@@ -1,7 +1,7 @@
 @props(['comments'])
 <section class="container">
     <div class="col-md-8 mx-auto">
-        <h3 class="my-3 text-secondary">Comments (3)</h3>
+        <h3 class="my-3 text-secondary">Comments ({{ $comments->count() }})</h3>
         @foreach ($comments as $comment)
             <x-single-comment :comment="$comment" />
         @endforeach
