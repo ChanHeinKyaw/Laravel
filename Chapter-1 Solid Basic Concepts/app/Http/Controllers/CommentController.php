73f9d976a8d3cd59e04f976a55b21b991c56a6c9
scmchanheinkyaw <scm.chanheinkyaw@gmail.com>
2023-07-22 21:52:14 +0630

    introduce to mail system

--- Chapter-1 Solid Basic Concepts/app/Http/Controllers/CommentController.php
+++ Chapter-1 Solid Basic Concepts/app/Http/Controllers/CommentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
+use App\Mail\SubscriberMail;
+use Illuminate\Support\Facades\Mail;
 
 class CommentController extends Controller
 {
@@ -15,6 +17,12 @@ class CommentController extends Controller
             'body' => request('body'),
             'user_id' => auth()->id(),
         ]);
+
+        $subscribers = $blog->subscribers->filter(fn ($subscriber) => $subscriber->id != auth()->user()->id);
+
+        $subscribers->each(function($subscriber) use ($blog){
+            Mail::to($subscriber->email)->send(new SubscriberMail($blog));
+        });
 
         return redirect('/blogs/' . $blog->slug);
     }
