c7f4a5d39dddda7bd618b0d9213208882815736f
scmchanheinkyaw <scm.chanheinkyaw@gmail.com>
2023-07-24 21:56:38 +0630

    real world component refactoring workshop

--- Chapter-1 Solid Basic Concepts/resources/views/blogs/create.blade.php
+++ Chapter-1 Solid Basic Concepts/resources/views/blogs/create.blade.php
@@ -4,40 +4,20 @@
         <x-card-wrapper>
             <form action="/admin/blogs/store" method="POST" enctype="multipart/form-data">
                 @csrf
-                <div class="mb-3">
-                    <label for="title" class="form-label">Title</label>
-                    <input type="text" name="title" class="form-control" id="title" value="{{ old('title') }}">
-                    <x-error name="title"/>
-                </div>
-                <div class="mb-3">
-                    <label for="slug" class="form-label">Slug</label>
-                    <input type="text" name="slug" class="form-control" id="slug" value="{{ old('slug') }}">
-                    <x-error name="slug"/>
-                </div>
-                <div class="mb-3">
-                    <label for="intro" class="form-label">Intro</label>
-                    <input type="text" name="intro" class="form-control" id="intro" value="{{ old('intro') }}">
-                    <x-error name="intro"/>
-                </div>
-                <div class="mb-3">
-                    <label for="body" class="form-label">Body</label>
-                    <textarea name="body" id="body" cols="30" rows="10" class="form-control">{{ old('body') }}</textarea>
-                    <x-error name="body"/>
-                </div>
-                <div class="mb-3">
-                    <label for="thumbnail" class="form-label">Thumbnail</label>
-                    <input type="file" name="thumbnail" class="form-control" id="thumbnail">
-                    <x-error name="thumbnail"/>
-                </div>
-                <div class="mb-3">
-                    <label for="category" class="form-label">Category</label>
+                <x-form.input name="title"/>
+                <x-form.input name="slug"/>
+                <x-form.input name="intro"/>
+                <x-form.textarea name="body"/>
+                <x-form.input name="thumbnail" type="file"/>
+                <x-form.input-wrapper>
+                    <x-form.label name="category"/>
                     <select name="category_id" id="category" class="form-control">
                         @foreach ($categories as $category)
                             <option {{ $category->id == old('category_id') ? 'selected' : '' }} value="{{ $category->id }}">{{ $category->name }}</option>
                         @endforeach
                     </select>
                     <x-error name="category_id"/>
-                </div>
+                </x-form.input-wrapper>
                 <div class="d-flex justify-content-start mt-3">
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
---
+++ Chapter-1 Solid Basic Concepts/resources/views/components/form/textarea.blade.php
@@ -0,0 +1,6 @@
+@props(['name'])
+<div class="mb-3">
+    <x-form.label name="{{ $name }}"/>
+    <textarea name="{{ $name }}" id="{{ $name }}" cols="30" rows="10" class="form-control">{{ old($name) }}</textarea>
+    <x-error name="{{ $name }}"/>
+</div>
